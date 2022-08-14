f308cc658bd9341121b9f78f4c75c91cd3249e05
Farhad Abedinzadeh Torghabeh <96732467+farhadabedinzadeh@users.noreply.github.com>
2022-08-15 00:50:25 +0430

    Update ufs_FMI.m

--- FMIUFS/ufs_FMI.m
+++ FMIUFS/ufs_FMI.m
@@ -1,3 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is the code for the Auto-UFSTool, which is an Automatic Unspervised %
+% Feature Selection Toolbox                                                %
+% Version 1.0 August 2022   |  Copyright (c) 2022   | All rights reserved  %
+%        ------------------------------------------------------            %
+% Redistribution and use in source and binary forms, with or without       %
+% modification, are permitted provided that the following conditions are   %
+% met:                                                                     %
+%    * Redistributions of source code must retain the above copyright      %
+%    * Redistributions in binary form must reproduce the above copyright.  %
+%         ------------------------------------------------------           %
+% "Auto-UFSTool,                                                           %
+%  An Automatic MATLAB Toolbox for Unsupervised Feature Selection"         %
+%         ------------------------------------------------------           %
+%                    Farhad Abedinzadeh | Yegane Modaresnia                %
+%          farhaad.abedinzade@gmail.com | y.modaresnia@yahoo.com           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 %%% The fuzzy mutual information-based unsupervised feature selection (FIMUFS) algorithm. 
 %%% Please refer to the following papers: 
 %%% Yuan Zhong, Chen Hongmei, Zhang Pengfei, Wan Jihong and Li Tianrui. A Novel Unsupervised 
