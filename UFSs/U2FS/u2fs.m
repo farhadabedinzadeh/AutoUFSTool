ee30b7854b642721b3d8d89febb4dbfde02517c1
Farhad Abedinzadeh Torghabeh <96732467+farhadabedinzadeh@users.noreply.github.com>
2022-08-16 01:11:19 +0430

    Update u2fs.m

--- UFSs/U2FS/u2fs.m
+++ UFSs/U2FS/u2fs.m
@@ -1,5 +1,21 @@
 function featsSelected = u2fs(data,numClus,numFeats2select,options)
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is the code for the Auto-UFSTool, which is an Automatic Unspervised %
+% Feature Selection Toolbox                                                %
+% Version 1.0 August 2022   |  Copyright (c) 2022   | All rights reserved  %
+%        ------------------------------------------------------            %
+% Redistribution and use in source and binary forms, with or without       %
+% modification, are permitted provided that the following conditions are   %
+% met:                                                                     %
+%    * Redistributions of source code must retain the above copyright      %
+%    * Redistributions in binary form must reproduce the above copyright.  %
+%         ------------------------------------------------------           %
+% "Auto-UFSTool,                                                           %
+%  An Automatic MATLAB Toolbox for Unsupervised Feature Selection"         %
+%         ------------------------------------------------------           %
+%                    Farhad Abedinzadeh | Yegane Modaresnia                %
+%          farhaad.abedinzade@gmail.com | y.modaresnia@yahoo.com           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  U2FS - Utility metric for Unsupervised feature selection
 %  
